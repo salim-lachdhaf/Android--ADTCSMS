f765eea133f9d73ca21dc8524b4f2f385171310e
Salim Lachdhaf <salim.sidratsoft@gmail.com>
2019-02-24 14:07:20 +0100

    v1.0.6- remove SMS permission

--- app/src/main/java/com/soft/salim/adtcsms/MainActivity.java
+++ app/src/main/java/com/soft/salim/adtcsms/MainActivity.java
@@ -1,23 +1,15 @@
 package com.soft.salim.adtcsms;
 
 
-import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -25,7 +17,6 @@ import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText ReceiverNumber;
     private EditText Param1;
     private EditText Param2;
     private EditText Msg;
@@ -140,7 +131,6 @@ public class MainActivity extends AppCompatActivity {
                 sendDialog();
             }
         });
-
     }
 
     public String EnvoyerMsg(String message) {
